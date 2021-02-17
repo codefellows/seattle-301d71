76a3510e32de8e338b38520dfab3407e417116aa
Nicholas Carignan <nick.carignan@sbcglobal.net>
2021-02-17 12:41:33 -0800

    day 3 code review

---
+++ class-03/code-review/horned-animal-lite/app.js
@@ -0,0 +1,209 @@
+const hornedPictureJson = [
+  {
+    'image_url': 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+    'title': 'UniWhal',
+    'description': 'A unicorn and a narwhal nuzzling their horns',
+    'keyword': 'narwhal',
+    'horns': 1
+  },
+  {
+    'image_url': 'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80',
+    'title': 'Rhino Family',
+    'description': 'Mother (or father) rhino with two babies',
+    'keyword': 'rhino',
+    'horns': 2
+  },
+  {
+    'image_url': 'https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg',
+    'title': 'Unicorn Head',
+    'description': 'Someone wearing a creepy unicorn head mask',
+    'keyword': 'unicorn',
+    'horns': 1
+  },
+  {
+    'image_url': 'https://images.unsplash.com/photo-1518946222227-364f22132616?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4836a6fca62e7dce9324346bacfde085&auto=format&fit=crop&w=2534&q=80',
+    'title': 'UniLego',
+    'description': 'Lego figurine dressed in a unicorn outfit',
+    'keyword': 'unilego',
+    'horns': 1
+  },
+  {
+    'image_url': 'https://i.pinimg.com/736x/b4/61/06/b46106830b841017ea59870b27ec18dc--narwhals-a-unicorn.jpg',
+    'title': 'Basically a unicorn',
+    'description': 'A narwhal is basically a unicorn after all, right?',
+    'keyword': 'narwhal',
+    'horns': 1
+  },
+  {
+    'image_url': 'https://i.pinimg.com/originals/16/cf/2a/16cf2a0b3fd51b9bee08bb6296193b75.jpg',
+    'title': '#truth',
+    'description': 'The truth behind narwhals',
+    'keyword': 'narwhal',
+    'horns': 1
+  },
+  {
+    'image_url': 'https://secure.img1-ag.wfcdn.com/im/17007094/resize-h800%5Ecompr-r85/3589/35892451/Baby+Rhino+Figurine.jpg',
+    'title': 'Baby Rhino',
+    'description': 'This is actually a figurine but it looks kinda real',
+    'keyword': 'rhino',
+    'horns': 2
+  },
+  {
+    'image_url': 'https://vignette.wikia.nocookie.net/landbeforetime/images/c/c3/Cera_infobox.png/revision/latest?cb=20180422005003',
+    'title': 'Cera',
+    'description': 'Three horns but still, horns. And who doesn\'t like The Land Before Time?',
+    'keyword': 'triceratops',
+    'horns': 3
+  },
+  {
+    'image_url': 'https://ae01.alicdn.com/kf/HTB18GwSQVXXXXaZaXXXq6xXFXXXh/Animal-Cosplay-Costume-Narwhal-Onesie-Mens-Womens-Cartoon-Whale-Pajamas.jpg',
+    'title': 'Narwhal costume',
+    'description': 'A woman wearing a blue narwhal costume',
+    'keyword': 'narwhal',
+    'horns': 1
+  },
+  {
+    'image_url': 'https://www.shopmascot.com/image/cache/mascotnew/new196-800x800.jpg',
+    'title': 'Rhino costume',
+    'description': 'Mascots have to get their costumes somewhere',
+    'keyword': 'rhino',
+    'horns': 2
+  },
+  {
+    'image_url': 'https://www.tinselbox.com/wp-content/uploads/2018/03/I-BELIEVE-IN-UNICORNS-FREE-PRINTABLE-WATERCOLOR-7-of-1.jpg',
+    'title': 'Believe',
+    'description': 'I believe in unicorns, do you?',
+    'keyword': 'unicorn',
+    'horns': 1
+  },
+  {
+    'image_url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Markhor_Schraubenziege_Capra_falconeri_Zoo_Augsburg-02.jpg/220px-Markhor_Schraubenziege_Capra_falconeri_Zoo_Augsburg-02.jpg',
+    'title': 'Markhor',
+    'description': 'These wild goats eat snakes, then secrete a foam that locals fight over for the antivemon properties -- how cool is that?',
+    'keyword': 'markhor',
+    'horns': 2
+  },
+  {
+    'image_url': 'http://www.zooborns.com/.a/6a010535647bf3970b0223c84d5959200c-800wi',
+    'title': 'Baby markhor',
+    'description': 'Even the babies are adorable',
+    'keyword': 'markhor',
+    'horns': 2
+  },
+  {
+    'image_url': 'https://images.unsplash.com/photo-1558560063-931ca9822a0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    'title': 'Mouflon',
+    'description': 'Those horns though',
+    'keyword': 'mouflon',
+    'horns': 2
+  },
+  {
+    'image_url': 'https://images.unsplash.com/photo-1556890077-020ec300d5db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80',
+    'title': 'Addax',
+    'description': 'This guy is basically extinct but survives well in captivity, so they\'re frequently found in zoos',
+    'keyword': 'addax',
+    'horns': 2
+  },
+  {
+    'image_url': 'https://cbsnews3.cbsistatic.com/hub/i/r/2013/03/05/5b414225-a645-11e2-a3f0-029118418759/thumbnail/620x350/2d4cf24685b45c22912e64d2004fec8d/Baby_Mouflon_Wild_Sheep.jpg',
+    'title': 'Baby mouflon',
+    'description': 'The cuteness that is a baby mouflon asleep',
+    'keyword': 'mouflon',
+    'horns': 2
+  },
+  {
+    'image_url': 'https://images.unsplash.com/photo-1514036783265-fba9577fc473?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    'title': 'Happy Jackson\'s Chameleon',
+    'description': 'These are really common in Hawaii',
+    'keyword': 'chameleon',
+    'horns': 2
+  },
+  {
+    'image_url': 'https://imgc.allpostersimages.com/img/print/posters/dlillc-jackson-s-chameleon_a-G-13448768-14258384.jpg',
+    'title': 'Serious Jackson\'s Chameleon',
+    'description': 'This one is very serious.',
+    'keyword': 'chameleon',
+    'horns': 3
+  },
+  {
+    'image_url': 'https://www.nps.gov/band/learn/nature/images/short-horned-lizard-open-mouth-18.jpg?maxwidth=650&autorotate=false',
+    'title': 'Horned Lizard',
+    'description': 'Fave food: ants',
+    'keyword': 'lizard',
+    'horns': 100
+  },
+  {
+    'image_url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Smaug_par_David_Demaret.jpg/290px-Smaug_par_David_Demaret.jpg',
+    'title': 'Smaug',
+    'description': 'Fan illustration of Smaug from \'The Hobbit\'',
+    'keyword': 'dragon',
+    'horns': 100
+  }
+];
+
+const hornedPictures = [];
+
+hornedPictureJson.forEach(picture => {
+  hornedPictures.push(new HornedImage(picture.title, picture.keyword));
+});
+
+function HornedImage (title, keyword){
+  this.title = title;
+  this.keyword = keyword;
+}
+
+HornedImage.prototype.render = function(){
+  const $li = $('#hide-me li');
+  const $liCopy = $li.clone();
+  $liCopy.find('h2').text(this.title + ' + ' + this.keyword);
+  // $liCopy.find('h2').text(this.title);
+  $liCopy.addClass(this.keyword);
+  $('ul').append($liCopy);
+};
+
+hornedPictures.forEach(pic => pic.render());
+
+function renderAllOptions(){
+  $('select').append('<option>pick a keyword to filter</option>');
+
+  hornedPictures.forEach(pic => {
+
+    // only add the option if it is not already in the select
+
+    //if(! isInTheSelect)
+    //if(! $('select:contains(keyword)'));
+    // console logged that selection and saw that equals app.js:174 S.fn.init [prevObject: S.fn.init(1)]
+    // that thing is truthy
+    // our ! is always !true === false
+    // we need to check something other than its truthiness,
+    // if its length is 0 that means it found nothing
+    // if its length is 1,2,3,4,5,6 etc it found that many duplicates
+
+    if($(`select:contains(${pic.keyword})`).length === 0){
+      $('select').append(`<option>${pic.keyword}</option>`);
+    }
+
+  });
+}
+/*
+Note: change your `template` to be any other type of tag
+Note: to hide and show: 1. get the keyword that was clicked on
+2. show just the li's that contain the keyword
+*/
+
+renderAllOptions();
+
+
+$('select').on('change', handleClickOnKeyword);
+
+function handleClickOnKeyword(){
+  const theKeywordIClickedOn = $('select').val();
+
+  showKeywordHideOthers(theKeywordIClickedOn);
+}
+
+function showKeywordHideOthers(keyword){
+  $('li').hide();
+  // $(`li:contains(${keyword})`).show();
+  $(`.${keyword}`).show();
+}
